9f9b5aee04c4b4b576b11cc92d511df7e982123d
VannTakashi <andreanrevan276@gmail.com>
2025-03-15 04:42:03 +0700

    drop duplication
    Signed-off-by: VannTakashi <andreanrevan276@gmail.com>

--- device.mk
+++ device.mk
@@ -199,8 +199,6 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub \
-    libmtkperf_client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
