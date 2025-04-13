9ac0819e98a17114b69d495affb362f763810fed
VannTakashi <andreanrevan276@gmail.com>
2025-04-13 22:26:24 +0700

    Add vendorsetup.sh
    For easily clone all stuffs needed for gale while running . build/envsetup.sh
    
    Signed-off-by: VannTakashi <andreanrevan276@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Clone Vendor tree
+git clone https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi_gale.git -b lineage-21 vendor/xiaomi/gale
+
+# Clone Kernel Tree
+git clone https://github.com/VannTakashi/kernel_xiaomi_gale kernel/xiaomi/gale
+
+# Clone Sepolicy vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
+
+# Clone Hardware supports mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-21 hardware/mediatek
+
+# Clone Xiaomi Hardware supports
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
