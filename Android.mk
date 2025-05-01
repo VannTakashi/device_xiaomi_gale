f69b8b127340a8fe3fb726d8611df2a85ed4bfe2
VannTakashi <ribrib32@honeys.be>
2025-05-01 11:05:47 +0000

    Major Improvement
    Signed-off-by: VannTakashi <ribrib32@honeys.be>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,32 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),gale)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+VENDOR_SYMLINKS := \
+    $(TARGET_OUT_VENDOR)/lib \
+    $(TARGET_OUT_VENDOR)/lib64 \
+    $(TARGET_OUT_VENDOR)/lib/hw \
+    $(TARGET_OUT_VENDOR)/lib64/hw \
+    $(TARGET_OUT_VENDOR)/bin/hw
+
+$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	$(hide) echo "Making vendor symlinks"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64
+        @mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+        @mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+        @mkdir -p $(TARGET_OUT_VENDOR)/bin/hw
+        @ln -sf /vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b $(TARGET_OUT_VENDOR)/bin/hw/android.hardware.media.c2@1.2-mediatek
+        @ln -sf /vendor/lib/mt6768/libpq_prot.so $(TARGET_OUT_VENDOR)/lib/libpq_prot.so
+        @ln -sf /vendor/lib/mt6768/libmtk_drvb.so $(TARGET_OUT_VENDOR)/lib/libmtk_drvb.so
+        @ln -sf /vendor/lib/mt6768/libdpframework.so $(TARGET_OUT_VENDOR)/lib/libdpframework.so
+        @ln -sf /vendor/lib64/mt6768/libdpframework.so $(TARGET_OUT_VENDOR)/lib64/libdpframework.so
+        @ln -sf /vendor/lib64/mt6768/libmtk_drvb.so $(TARGET_OUT_VENDOR)/lib64/libmtk_drvb.so
+        @ln -sf /vendor/lib64/mt6768/libpq_prot.so $(TARGET_OUT_VENDOR)/lib64/libpq_prot.so
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	$(hide) touch $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
+
 endif
