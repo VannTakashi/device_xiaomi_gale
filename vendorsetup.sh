add58a87e29e0a029cdcf0cab1d7512f7b7de0de
VannTakashi <andreanrevan276@gmail.com>
2025-06-07 20:22:21 +0700

    gale: rebase vemdorsetup.sh and add aosp clangs
    Signed-off-by: VannTakashi <andreanrevan276@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,13 @@
+# Add Clang For Compile Kernel
+cd prebuilts/clang/host/linux-x86
+mkdir clang-r487747c ; cd clang-r487747c ; wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android14-release/clang-r487747c.tar.gz ; tar -xf * && ls
+cd ../../../../..
+
+# Clone KernelTree
+https://github.com/VannTakashi/kernel_xiaomi_gale kernel/xiaomi/gale
+
 # Clone Vendor tree
-git clone https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi_gale.git -b lineage-21 vendor/xiaomi/gale
+git clone https://github.com/VannTakashi/vendor_xiaomi_gale.git -b dev/lineage-20 vendor/xiaomi/gale
 
 # Clone Sepolicy vndr
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
