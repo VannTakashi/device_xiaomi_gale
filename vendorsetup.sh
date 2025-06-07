dac0604ac8156a1fc20fd231b0086a3252306852
VannTakashi <andreanrevan276@gmail.com>
2025-06-08 02:02:11 +0700

    Horizonify
    Signed-off-by: VannTakashi <andreanrevan276@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+# Clone KernelTree
+https://github.com/VannTakashi/kernel_xiaomi_gale kernel/xiaomi/gale
+
+# Clone Vendor tree
+git clone https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi_gale.git -b lineage-22.2 vendor/xiaomi/gale
+
+# Clone Sepolicy vndr
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
+
+# Clone Hardware supports mediatek
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+
+# Clone Xiaomi Hardware supports
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
