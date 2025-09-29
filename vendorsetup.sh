f2a104a12e33b244b139e837f09bde55a4cf4ff1
vannxavero <vannxaveroo@proton.me>
2025-09-29 21:06:20 +0700

    Evolving
    Signed-off-by: vannxavero <vannxaveroo@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 # Clone Vendor tree
-git clone https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi_gale.git -b lineage-21 vendor/xiaomi/gale
+git clone https://github.com/VannTakashi/vendor_xiaomi_gale.git -b lineage-21 vendor/xiaomi/gale
 
 # Clone Kernel Tree
-git clone https://github.com/VannTakashi/kernel_xiaomi_gale kernel/xiaomi/gale
+git clone https://github.com/VannTakashi/kernel_xiaomi_gale -b los-21 kernel/xiaomi/gale
 
 # Clone Sepolicy vndr
 git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-21 device/mediatek/sepolicy_vndr
